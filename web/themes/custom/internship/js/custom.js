e18a7c8af5c3bf8b5d7ccd736b8baa950bec8e34
Vasilije Tomovic <vasilijetomovic98@gmail.com>
2020-12-01 12:38:47 +0100

    Added internship (barrio sass) and vaske (barrio) subtheme

---
+++ web/themes/custom/internship/js/custom.js
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.internship = {
+    attach: function(context, settings) {
+
+      // Custom code here
+
+    }
+  };
+
+})(jQuery, Drupal);
